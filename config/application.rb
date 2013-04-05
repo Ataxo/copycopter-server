379d5bf42d233385ed1d8df9e53ff2714d0e7a0e
Ondrej Bartas <ondrej@bartas.cz>
2013-04-05 16:47:54 +0200

    [ADD] External/Internal encoding to UTF-8

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+Encoding.default_internal = "UTF-8"
+Encoding.default_external = "UTF-8"
+
 if defined? Bundler
   Bundler.require *Rails.groups(:assets => %w(development test))
 end
